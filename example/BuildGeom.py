11814eb58ff4f0cfd7f940d41e791d54a55c294a
Tony Alberti <albertia@oregonstate.edu>
2016-09-15 13:22:59 -0600

    refined mesh to try and remove numerical round off issues with simulation

--- example/BuildGeom.py
+++ example/BuildGeom.py
@@ -1,4 +1,5 @@
 #!python
+#!python
 cubit.cmd('reset')
 
 cubit.cmd('create surface rectangle width 30 height 30 zplane ')
@@ -9,10 +10,10 @@ cubit.cmd('compress all')
 cubit.cmd('imprint all')
 cubit.cmd('merge all')
 
-cubit.cmd('surface 1 size auto factor 4')
+cubit.cmd('surface 1 size auto factor 2')
 cubit.cmd('surface 1 scheme TriMesh')
 cubit.cmd('mesh surface 1')
-cubit.cmd('surface 1 size auto factor 5')
+cubit.cmd('surface 1 size auto factor 3')
 cubit.cmd('surface 2 scheme TriMesh')
 cubit.cmd('mesh surface 2')
 
@@ -32,4 +33,5 @@ dum_mesh = cubit.get_mesh_error_count()
 print('\n\nNUMBER OF RECORDED MESH ERRORS: '+str(dum_mesh)+'\n')
 
 cubit.cmd('set large exodus file off')
-cubit.cmd('export mesh "test1.e" dimension 2 overwrite')
+cubit.cmd('export mesh "./test1.e" dimension 2 overwrite')
+
